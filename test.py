6459e5e34aa38616198430f9f02631faee5362bc
JeongHyunAn <77708843+JeongHyunAn@users.noreply.github.com>
2024-07-01 14:56:47 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+import random
+
+# a = defaultdict(list)
+# a = {'a' : [1,2,3,], 'b' : [4,5,6], 'c' : [7,8,9]}
+
+# emp_floor = [];
+
+# for i in range(30):
+#     floor = random.randint(1, 10);
+#     emp_floor.append(floor);
+
+# for i in range(30):
+#     if emp_floor[i] == emp_floor[3]:
+#         print(emp_floor[i])
+
+scan_list = list(range(1, 51));
+print(scan_list)
